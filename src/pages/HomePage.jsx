d1b277b731b613bd90f20fb671847c6c6ac7e8f2
DeraTheOz <emmanuelchi545@gmail.com>
2025-10-13 18:17:40 +0100

    feat: add SearchBar and Filter component to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,13 @@
+import Filter from "../ui/Filter";
+import SearchBar from "../ui/SearchBar";
+
 function HomePage() {
-  return <div>Home</div>;
+  return (
+    <section className="flex flex-col justify-between gap-8 px-4 sm:flex-row sm:px-10">
+      <SearchBar />
+      <Filter />
+    </section>
+  );
 }
 
 export default HomePage;
