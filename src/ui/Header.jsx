3544404e94c918cec7a87ebb9e70b9daaae1b2f0
DeraTheOz <emmanuelchi545@gmail.com>
2025-10-13 18:08:59 +0100

    feat: add header component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,11 @@
-import ThemeSwitcher from "./ThemeSwitcher";
+import ThemeToggle from "./ThemeToggle";
 
 function Header() {
   return (
-    <header className="dark:shadow-[ 0 2px 4px 0 rgba(0, 0, 0, 0.06)] mb-10 flex h-20 items-center justify-between bg-white px-4 shadow-sm md:px-10 dark:bg-blue-900">
-      <h1 className="font-extrabold md:text-2xl">Where in the world?</h1>
+    <header className="mb-6 flex h-16 items-center justify-between bg-white px-4 shadow-sm sm:mb-20 sm:px-10 dark:bg-blue-900">
+      <h1 className="font-extrabold sm:text-2xl">Where in the world?</h1>
 
-      <ThemeSwitcher />
+      <ThemeToggle />
     </header>
   );
 }
