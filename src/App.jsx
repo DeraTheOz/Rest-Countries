56a1107b31bd0d7cf181e1721c423df68154ff25
DeraTheOz <emmanuelchi545@gmail.com>
2026-03-27 15:50:21 +0100

    feat: add country details loader and skip home revalidation checks on search param updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,29 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 
 import HomePage, { loader } from "./pages/HomePage";
-import CountryDetailsPage from "./pages/CountryDetailsPage";
+import CountryDetailsPage, {
+  loader as countryDetailsLoader,
+} from "./pages/CountryDetailsPage";
 
 import AppLayout from "./ui/AppLayout";
 import PageNotFound from "./ui/PageNotFound";
+
+function shouldRevalidateHomePage({
+  currentUrl,
+  nextUrl,
+  defaultShouldRevalidate,
+}) {
+  const isHomeSearchParamNavigation =
+    currentUrl.pathname === "/" &&
+    nextUrl.pathname === "/" &&
+    currentUrl.search !== nextUrl.search;
+
+  if (isHomeSearchParamNavigation) {
+    return false;
+  }
+
+  return defaultShouldRevalidate;
+}
 
 const router = createBrowserRouter([
   {
@@ -16,11 +35,13 @@ const router = createBrowserRouter([
       {
         index: true,
         loader: loader,
+        shouldRevalidate: shouldRevalidateHomePage,
         Component: HomePage,
       },
 
       {
         path: "/country/:countryName",
+        loader: countryDetailsLoader,
         Component: CountryDetailsPage,
       },
     ],
