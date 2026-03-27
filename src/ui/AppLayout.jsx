df4ca18ba94091510c745a9fbd4ffadac15b8346
DeraTheOz <emmanuelchi545@gmail.com>
2026-03-27 15:53:58 +0100

    refactor: remove loading state effect, search and region components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,31 +1,10 @@
 import { Outlet } from "react-router";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import Header from "./Header";
-import SearchBar from "./SearchBar";
-import Filter from "./Filter";
-
-import { setStatus } from "../features/countries/countriesSlice";
 
 function AppLayout() {
-  const dispatch = useDispatch();
-  const { status } = useSelector((store) => store.countries);
-
-  // Set loading state on app mount
-  useEffect(() => {
-    dispatch(setStatus("loading"));
-  }, [dispatch]);
-
   return (
     <>
       <Header />
-
-      {status !== "failed" && (
-        <section className="mx-auto mb-8 flex max-w-[90rem] flex-col justify-between gap-8 px-4 sm:flex-row sm:px-10">
-          <SearchBar />
-          <Filter />
-        </section>
-      )}
 
       <main className="mx-auto mb-10 max-w-[90rem] px-4 sm:px-10">
         <Outlet />
