12f823c4ce20813e41be61e0986e7b1af05e3158
DeraTheOz <emmanuelchi545@gmail.com>
2026-03-27 16:18:05 +0100

    refactor: update imports

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -11,17 +11,14 @@ import {
 } from "../features/countries/countriesSlice";
 
 import {
-  findCountryByName,
-  getBorderCountries,
   getCountryPath,
-} from "../features/countries/countriesUtils";
-
-import {
   getCapital,
   getCurrencies,
   getLanguages,
   getNativeName,
   getTopLevelDomain,
+  getBorderCountries,
+  findCountryByName,
   loadCountryPageData,
 } from "../features/countries/countryDetailsUtils";
 
@@ -185,15 +182,15 @@ function CountryDetailsPage() {
         Back
       </Button>
 
-      <div className="mt-16 grid grid-cols-1 xl:grid-cols-2 xl:items-center xl:gap-16">
-        <div className="overflow-clip rounded-lg">
+      <div className="grid grid-cols-1 mt-16 xl:grid-cols-2 xl:items-center xl:gap-16">
+        <div className="rounded-lg overflow-clip">
           <img
             src={displayedCountry.flags.svg}
             alt={displayedCountry.flags.alt || `${countryName} flag`}
           />
         </div>
 
-        <div className="mb-4 mt-12 flex flex-col xl:m-0">
+        <div className="flex flex-col mt-12 mb-4 xl:m-0">
           <h2 className="mb-8 text-[1.375rem] font-extrabold lg:text-[2rem]">
             {countryName}
           </h2>
@@ -230,7 +227,7 @@ function CountryDetailsPage() {
             </ul>
           </div>
 
-          <div className="mt-8 flex flex-col gap-4 xs:flex-row xs:items-start xl:mt-16">
+          <div className="flex flex-col gap-4 mt-8 xs:flex-row xs:items-start xl:mt-16">
             <p className="font-semibold">Border Countries:</p>
 
             {borderCountries.length > 0 ? (
